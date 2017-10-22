044d168075e5297ad57a1840b797d94b184c94f5
estemomento <estemomento@qq.com>
2017-10-22 11:28:17 +0800

    Delete unused packages, fix structure

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('test')
+});
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id
+  res.send(id)
 });
 
 module.exports = router;
