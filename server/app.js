62786c9ac4a4a0e0de55da17f6abf2d06ca725de
estemomento <estemomento@qq.com>
2017-10-22 17:29:17 +0800

    delete router, use original route

--- server/app.js
+++ server/app.js
@@ -1,22 +1,35 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var mongo = require('mongodb').MongoClient
+var assert = require('assert')
 
-var data = require('./routes/data');
-
-var app = express();
+var app = express()
 
 // uncomment after placing your favicon in /bin
-//app.use(favicon(path.join(__dirname, 'bin', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'bin', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/data', data);
+app.get('/data/:id', function (req, res, next) {
+  var id = req.params.id
+  var result = ''
+  var url = 'mongodb://localhost:27017/trialect'
+  mongo.connect(url, function (err, db) {
+    assert.equal(null, err)
+    console.log('Request: ' + id)
+    db.collection('trialect').find({'name': id}).toArray(function (err, docs) {
+      assert.equal(err, null)
+      res.send(docs[0])
+    })
+      db.close()
+  })
+})
 
-module.exports = app;
+module.exports = app
